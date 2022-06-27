d3a26119b4320ce4403044651f4b618a43f07747
geisondossantos <geison_bf@hotmail.com>
2022-06-27 17:13:42 -0300

    Adiciona aula 19 e 20

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/A0018-webpack/index.ts',
+  entry: './src/A0020-this-em-funcoes/index.ts',
   module: {
     rules: [
       {
