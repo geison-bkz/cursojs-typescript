ce8614e59c31140ef7b28a991c6ee0be2c4ce23a
geisondossantos <geison_bf@hotmail.com>
2022-07-04 18:24:29 -0300

    Aula 49

--- src/secao-21-TypeScript-Generics/A048-generics-6/A048-generics-6.ts
+++ src/secao-21-TypeScript-Generics/A048-generics-6/A048-generics-6.ts
@@ -1,4 +1,4 @@
-function unirObjetos<O1, O2>(obj1: O1, obj2: O2): O1 & O2 {
+export function unirObjetos<O1, O2>(obj1: O1, obj2: O2): O1 & O2 {
   //return { ...obj1, ...obj2 };
   return Object.assign({}, obj1, obj2);
 }
